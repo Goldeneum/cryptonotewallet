70512f66eccdaf58a7b33b9587e69e7245afb67e
Goldeneum <36080452+Goldeneum@users.noreply.github.com>
2018-02-16 10:58:45 +0000

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "Goldeneumwallet")
+set(CN_CURRENCY_DISPLAY_NAME "Goldeneum")
+set(CN_CURRENCY_TICKER "GOL")
